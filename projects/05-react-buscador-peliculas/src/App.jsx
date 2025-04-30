b68a2363f8acad3663d7262f51deea706fc31e01
Jesus Pimentel <jesus.pimentel@utp.ac.pa>
2025-04-29 22:23:17 -0500

    Practica 05 inicializacion y avance 1

--- projects/05-react-buscador-peliculas/src/App.jsx
+++ projects/05-react-buscador-peliculas/src/App.jsx
@@ -1,24 +1,26 @@
 import './App.css'
-import { useRef } from 'react'
+import { useRef, useState } from 'react'
 import {Movies} from './Components/Movies'
 import { useMovies } from './hooks/useMovies'
 function App() {
   const {movies}= useMovies()
   const inputRef = useRef()
+  const[query,setQuery]=useState('')
   const handleSubmit = (event)=>{
     event.preventDefault()
-    //Estas lineas de abajo lo que hacen es guardar la referencia del nombre del
-    //  input asi podemos manejar los input sin abusar del inputRef
-    const data = new FormData(event.target)
-    const query = data.get('query')
-    console.log(query)
+    console.log({query})
   }
+
+  const handleChange= (event) =>{
+    setQuery(event.target.value)
+  }
+
   return (
     <div className='page'>
       <header>
         <h1>Buscador de peliculas</h1>
       <form className='form' onSubmit={handleSubmit}>
-        <input name='query' ref={inputRef} placeholder='Avengers,Star Wars, Matrix...'/> 
+        <input onChange={handleChange} value={query} name='query' ref={inputRef} placeholder='Avengers,Star Wars, Matrix...'/>
         <button  type='submit'>Buscar</button>
       </form>
       </header>
