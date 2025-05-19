b1b161aa8059c8ec9c54e8b00ad8afc79c0bb432
Jesus Pimentel <jesus.pimentel@utp.ac.pa>
2025-05-18 22:53:50 -0500

    Practica 05 inicializacion y avance 5

--- projects/05-react-buscador-peliculas/src/App.jsx
+++ projects/05-react-buscador-peliculas/src/App.jsx
@@ -1,37 +1,59 @@
+import { useCallback, useState } from 'react'
 import './App.css'
-import {Movies} from './Components/Movies'
+import { Movies } from './Components/Movies'
 import { useMovies } from './hooks/useMovies'
 import { useSearch } from './hooks/useSearch'
+import debounce from 'just-debounce-it'
 
 function App() {
-  const {query,setQuery,error} = useSearch()
-  const {movies,loading,getMovies}= useMovies({query})
-  
-  const handleSubmit = (event)=>{
+  const [sort, setSort] = useState(false)
+  const { query, setQuery, error } = useSearch()
+  const { movies, loading, getMovies } = useMovies({ query, sort })
+
+  const debounceGetMovies = useCallback(
+    debounce((query) => {
+      console.log('search', query)
+      getMovies({ query })
+    }, 2000),
+    [getMovies]
+  )
+
+  const handleSubmit = (event) => {
     event.preventDefault()
-    getMovies()
+    getMovies({ query })
   }
 
   const handleChange = (event) => {
-    setQuery(event.target.value)
+    const newQuery = event.target.value
+    setQuery(newQuery)
+    debounceGetMovies(newQuery)
   }
-  
+
+  const handleSort = () => {
+    setSort(!sort)
+  }
+
   return (
     <div className='page'>
       <header>
-        <h1>Buscador de peliculas</h1>
-      <form className='form' onSubmit={handleSubmit}>
-        <input onChange={handleChange} value={query} name='query'  placeholder='Avengers,Star Wars, Matrix...'/>
-        <button  type='submit'>Buscar</button>
-      </form>
-      {error && <p style={{color: 'red'}}>{error}</p>}
+        <h1>Buscador de películas</h1>
+        <form className='form' onSubmit={handleSubmit}>
+          <input
+            onChange={handleChange}
+            value={query}
+            name='query'
+            placeholder='Avengers, Star Wars, Matrix...'
+          />
+          <input type='checkbox' onChange={handleSort} checked={sort} />
+          <button type='submit'>Buscar</button>
+        </form>
+        {error && <p style={{ color: 'red' }}>{error}</p>}
       </header>
       <main>
-        {
-          loading ? <p>Cargando....</p>: <Movies movies={movies}/>
-        }
+        {loading ? <p>Cargando....</p> : <Movies movies={movies} />}
       </main>
     </div>
   )
 }
+
 export default App
--- projects/05-react-buscador-peliculas/src/hooks/useMovies.js
+++ projects/05-react-buscador-peliculas/src/hooks/useMovies.js
@@ -1,23 +1,36 @@
-import { useState } from 'react'
+import { useState, useRef, useMemo, useCallback } from 'react'
 import { searchMovie } from '../services/movie'
-//Esta funcion que contiene el mapepedMovies lo que hacemos es intancear los datos de la app a este componente
-//para poder manejarlos nosotros mismos y no depender del contrato directo de la api
-export function useMovies ({query}){
-  const [movies,setMovie]= useState([])
-  const [loading,setLoading]= useState([false])
-  const [error,setError] = useState([null])
-    const getMovies= async()=>{
-      try{
-        setLoading(true)
-        setError(null)
-        const newMovie = await searchMovie ({query})
-        setMovie(newMovie)
-      }catch(e){
-        setError(e.message)
-      }finally{
-        setLoading(false)
-      }
-    }
 
-    return {movies,loading,getMovies}
-  }
+export function useMovies({ query, sort }) {
+  const [rawMovies, setRawMovies] = useState([]) // aquí guardas las pelis originales
+  const [loading, setLoading] = useState(false)
+  const [error, setError] = useState(null)
+  const previousSearch = useRef(query)
+
+  const getMovies = useCallback(async () => {
+    if (query === previousSearch.current) return
+    try {
+      setLoading(true)
+      setError(null)
+      previousSearch.current = query
+      const newMovies = await searchMovie({ query })
+      setRawMovies(newMovies)
+    } catch (e) {
+      setError(e.message)
+    } finally {
+      setLoading(false)
+    }
+  }, [query])
+
+
+
+const sortedMovies =useMemo(()=>{ 
+  return sort 
+  ? [...rawMovies].sort((a, b) => a.title.localeCompare(b.title))
+  : rawMovies
+},[sort, rawMovies])
+ return { movies: sortedMovies, loading, getMovies, error }
+}
+
+//recuerda que las funciones se renderisan en el caso de los hooks es diferentes porque tienen sus reglas
+ 
